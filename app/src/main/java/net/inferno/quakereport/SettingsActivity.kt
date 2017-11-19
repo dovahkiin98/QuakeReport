102484277fe956ecbfd08dac5c3e9ed2bda1d81b
Ahmad Sattout <dovahkiin98@users.noreply.github.com>
2017-11-19 09:51:12 +0200

    Added Location Service and Settings

--- app/src/main/java/net/inferno/quakereport/SettingsActivity.kt
+++ app/src/main/java/net/inferno/quakereport/SettingsActivity.kt
@@ -29,8 +29,8 @@ class SettingsActivity : AppCompatActivity() {
             val location = findPreference(getString(R.string.location_key)) as SwitchPreference
 
             minMagnitude.setOnPreferenceChangeListener { _, newValue -> minMagnitude.summary = newValue.toString(); true }
-            orderBy.setOnPreferenceChangeListener { listPreference, _ ->
-                orderBy.summary = (listPreference as ListPreference).entry
+            orderBy.setOnPreferenceChangeListener { _, newValue ->
+                orderBy.summary = newValue.toString()
                 true
             }
             location.setOnPreferenceClickListener {
